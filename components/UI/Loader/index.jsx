308473ccf02d4948e3b59d0bfbc669805f952064
David Alamu <aestrex@gmail.com>
2021-12-08 03:53:45 +0100

    fix web console errors

--- components/UI/Loader/index.jsx
+++ components/UI/Loader/index.jsx
@@ -10,7 +10,7 @@ const BitNobLoader = React.forwardRef(({ size = "sm" }, ref)=> {
 
     return(
         <svg ref={ref} className={`animate-spin inline text-white ${variants[size]} `} viewBox="0 0 24 24">
-            <circle className="opacity-0" cx="12" cy="12" r="10" stroke="primary" stroke-width="4"></circle>
+            <circle className="opacity-0" cx="12" cy="12" r="10" stroke="primary" strokeWidth="4"></circle>
             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
     )
--- public/svgs/bolt.js
+++ public/svgs/bolt.js
@@ -1,8 +1,8 @@
 
 const Bolt = props => (
     <svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M31.839 9.87645C17.9134 7.29375 12.4266 10.981 9.89662 24.6224C7.36663 38.2638 11.1662 43.6734 25.0918 46.2561C39.0174 48.8388 44.5041 45.1515 47.0341 31.5101C49.5641 17.8687 45.7645 12.4592 31.839 9.87645Z" stroke="#23E0A2" stroke-width="3.0625" stroke-linecap="round" stroke-linejoin="round" />
-        <path d="M31.3993 20.473C31.3993 20.473 26.4014 25.4856 26.0075 27.6094C25.8358 28.5354 29.4164 28.0926 30.1585 29.9049C30.5085 30.7595 26.2059 34.7656 26.2059 34.7656" stroke="#23E0A2" stroke-width="3.0625" stroke-linecap="round" stroke-linejoin="round" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M31.839 9.87645C17.9134 7.29375 12.4266 10.981 9.89662 24.6224C7.36663 38.2638 11.1662 43.6734 25.0918 46.2561C39.0174 48.8388 44.5041 45.1515 47.0341 31.5101C49.5641 17.8687 45.7645 12.4592 31.839 9.87645Z" stroke="#23E0A2" strokeWidth="3.0625" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M31.3993 20.473C31.3993 20.473 26.4014 25.4856 26.0075 27.6094C25.8358 28.5354 29.4164 28.0926 30.1585 29.9049C30.5085 30.7595 26.2059 34.7656 26.2059 34.7656" stroke="#23E0A2" strokeWidth="3.0625" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
 
 )
