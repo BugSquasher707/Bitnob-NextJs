f538af2871f4d34ca9c7446fd0b55ec93649a739
Vincent Ayorinde <vincentayorinde@yahoo.com>
2022-02-02 15:26:49 +0100

    update sitemap

--- pages/_app.js
+++ pages/_app.js
@@ -9,36 +9,57 @@ import { useRouter } from 'next/dist/client/router'
 import { useEffect } from 'react'
 import * as gtag from 'lib/gtag'
 
-
 function BitNob({ Component, pageProps }) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+	const router = useRouter()
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url)
+		}
+		router.events.on('routeChangeComplete', handleRouteChange)
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange)
+		}
+	}, [router.events])
 	const getLayout = Component.getLayout || ((page) => page)
 	return getLayout(
 		<>
 			<SEO
 				title='Welcome to Bitnob'
 				description='Bitnob is a delightful way to save, borrow, pay, receive, and get paid in Bitcoin on time'
-				keywords={['Savings', 'Bitcoin', 'DCA', 'Dollar Cost Averaging', 'Lightning Network', 'Lightning', 'USD Savings', 'Bitcoin Savings',' Automatically Save in Bitcoin', 'Buy', 'Sell', 'Send', 'Receive', 'Virtual Cards', 'Loans', 'Bitcoin Loans', 'APIs', 'Payments', 'Wallet', 'Accept payment in bitcoin', 'Remittance']}
+				keywords={[
+					'Savings',
+					'Bitcoin',
+					'DCA',
+					'Dollar Cost Averaging',
+					'Lightning Network',
+					'Lightning',
+					'USD Savings',
+					'Bitcoin Savings',
+					'Automatically Save in Bitcoin',
+					'Buy',
+					'Sell',
+					'Send',
+					'Receive',
+					'Virtual Cards',
+					'Loans',
+					'Bitcoin Loans',
+					'APIs',
+					'Payments',
+					'Wallet',
+					'Accept payment in bitcoin',
+					'Remittance',
+				]}
 				icon='https://bitnob.com/images/bitnob_ico.ico'
 				twitter={{
 					card: 'summary',
 					site: '@bitnob_official',
 					image: 'https://bitnob.com/images/featured_image_bitnob.png',
 				}}
-        facebook={{
-          image: 'https://bitnob.com/images/featured_image_bitnob.png',
+				facebook={{
+					image: 'https://bitnob.com/images/featured_image_bitnob.png',
 					url: 'https://bitnob.com',
-					type: 'website'
-        }}
+					type: 'website',
+				}}
 			/>
 			<Component {...pageProps} />
 		</>,
